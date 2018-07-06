fd9565bc1aeb0c13ba3d710f0d762b1eb8b2ef52
Amin Husni <aminhusni@users.noreply.github.com>
2018-07-06 15:23:57 +0800

    F

---
+++ arduino.ino
@@ -0,0 +1,109 @@
+// Count the number of the short and long words
+// Send to Pi after 5 secs silence
+// If GPIO is HIGH, despense drink
+// Written by Stanley Seow
+
+#include <Servo.h>
+
+Servo myservo;
+
+int i = 0;
+bool serialPlotter = 0;
+unsigned long lastCheck;
+unsigned long lastCheck2;
+unsigned int word1;
+unsigned int word2;
+unsigned int soundCheck;
+unsigned int passed;
+unsigned int failed;
+
+#define RED 9
+#define GRN 10
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Detecting Sound Peaks V5");
+
+  // Servo attached is pin11
+  myservo.attach(11);
+
+  // Move servo a bit to indicate servo is working
+  myservo.write(0);
+  delay(500);
+  myservo.write(30);
+  delay(500);
+  myservo.write(0);
+
+}
+
+void loop() {
+
+  // Beginning of Start Voice Detection
+  if ( digitalRead ( 2 ) == HIGH ) {
+
+  // read the input on analog pin 0:
+  int sensor = analogRead(A0);
+
+  int sound = sensor - 400;
+//  Serial.print(sound);
+//  Serial.println();
+
+  if ( sound < 90 || sound > 120  ) {
+        i++;
+        //Serial.print("Peak ");
+  }
+  delay(50);
+
+  while ( millis() - lastCheck > 1000 ) {
+
+    //Serial.println("1 sec checking...");
+    //Serial.println(i);
+
+//    if ( i >= 107 && i <= 110  ) {
+//      Serial.println("Silence");
+//      silence1++;
+//      Serial.print("Silence :");
+//      Serial.println(silence1++);
+//    }
+    
+    if ( i >= 1 && i < 4 ) {
+      //Serial.println("Short word");
+      word1++;
+    } else if ( i >= 4 && i < 10 ) {
+      //Serial.println("Long word");
+      word2++;
+    }
+
+    i = 0;
+    lastCheck = millis();
+    soundCheck++;
+  }
+
+  // Send to Pi after 7 secs
+  if ( soundCheck > 6  ) {
+    // Serial.println("*** 7 sec checking...");
+    // Print to RPI short words, long words
+    Serial.print(word1);
+    Serial.print(",");
+    Serial.println(word2);
+    word1 = 0;
+    word2 = 0;
+    soundCheck = 0;
+    
+  }
+
+  } // End of Start Voice Detection
+
+
+  // If D3 is HIGH, dispense drink
+  if ( digitalRead( 3 ) == HIGH ) {
+
+    myservo.write(180);
+    delay(5000);
+    myservo.write(0);
+    delay(5000);
+    Serial.println("*** Can dispensed");
+  }
+
+} // End of loop
+
---
+++ main.py
@@ -0,0 +1,63 @@
+import threading
+import tkinter as tk
+import tkinter.font
+import serial
+from omxplayer.player import OMXPlayer
+from pathlib import Path
+from time import sleep
+
+win = tk.Tk()
+win.title("Greyhound Pepsi")
+#win.attributes("-fullscreen", True)
+myFont=tkinter.font.Font(family='Helvetica',size=12,weight="bold")
+serial=serial.Serial('/dev/ttyUSB0',9600)
+
+videovar="null" #Determines the video playing
+
+FULL=Path("/home/pi/Desktop/projectvideo/full.mp4")
+player1=OMXPlayer(FULL,args=["--orientation","90","--loop","--no-osd"],dbus_name='org.mpris.MediaPlayer2.omxplayer0')
+
+
+bmbutton = tk.Button(win, text='BM', font=myFont, command=bm, height=90, width=90)
+bmbutton.grid(row=1, column=2, sticky=tk.NSEW)
+enbutton = tk.Button(win, text='EN', font=myFont, command=bm, height=90, width=90)
+enbutton.grid(row=1, column=2, sticky=tk.NSEW)
+
+
+def looper(starttime,videoname,endtime):
+ print("Looper active")
+ print("Video name: "+videoname)
+ print("Start pos: "+str(starttime))
+ global videovar
+ while(True):
+  sleep(0.01)
+  currentvidtime=player1.position()
+  if(currentvidtime>=endtime):
+   print("Relooping back to:"+str(starttime))
+   player1.set_position(starttime)
+  if(videoname!=videovar):
+   print("Video changed!")
+   break
+
+def seeking():
+ global videovar
+ print("Seeking thread started")
+ sleep(1)
+ print(videovar)
+ while(True):
+  sleep(1)
+  print("From seeking thread:")
+  print(videovar)
+  if(videovar=="language"):
+   print("video set to language")
+   starttime=0
+   duration=19
+   player1.set_position(starttime)
+   endtime=starttime+duration
+   looper(starttime,"language",endtime)
+
+def mainseries():
+    videovar = "language"
+
+if __name__ == '__main__':
+    win.mainloop()
