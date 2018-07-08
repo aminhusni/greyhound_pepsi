155c68b7d2017f0e4cf5620d3ee7236e84238510
Amin Husni <aminhusni@gmail.com>
2018-07-08 15:27:28 +0000

    d

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def en():
         attempts += 1
         print("Attempt: "+str(attempts))
         if(attempts >= 3):
-            sleep(4)
+            sleep(3)
             arduino.write(b'd')
             videovar = "dispense"
             sleep(11)
